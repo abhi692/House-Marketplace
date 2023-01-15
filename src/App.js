6dcdc1bca444379febb857419cb8c164be6ddf34
Abhishek Kumar <abhiroshan692@gmail.com>
2023-01-15 17:57:33 +0530

    profile updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Explore from "./pages/Explore";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar";
@@ -16,7 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Explore/>}/>
         <Route path="/offers" element={<Offers/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/profile" element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
